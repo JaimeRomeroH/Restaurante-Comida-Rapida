0c8634c085469c1666ec5a462b5943128f2bb5f7
JaimeRomero <jaime_im@outlook.com>
2026-08-05 21:48:15 -0600

    Se agrega la funcionalidad para validar el formulario de contacto

--- js/app.js
+++ js/app.js
@@ -34,3 +34,26 @@ btnDerecho.addEventListener("click", function(){
 btnIzquierdo.addEventListener("click", function(){
    contenedor.scrollLeft -= contenedor.offsetWidth;   
 });
+
+//Validacion de formulario
+var formulario = document.getElementById("formulario");
+
+function validar(e){
+    var inputNombre = document.getElementById("nombre"),
+        inputEmail = document.getElementById("email"),
+        inputComments = document.getElementById("comentarios");
+
+    if(inputNombre.value == 0 || inputEmail.value == 0 || inputComments.value == 0){
+        e.preventDefault();
+        alert("Datos vacios");
+    }else{
+        e.preventDefault();
+        alert("Datos enviados");
+        inputNombre.value = "";
+        inputEmail.value = "";
+        inputComments.value = "";
+    }
+}
+
+//Eventos del formulario
+formulario.addEventListener("submit", validar);
